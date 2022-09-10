f7a0f3ab95237fcf87db945540d38fa369c8d159
Hunter Dobbelmann <hunterdobbapps@gmail.com>
2022-09-10 12:48:16 -0400

    Initial Commit

---
+++ WaitTimes/WaitTimesApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaitTimesApp.swift
+//  WaitTimes
+//
+//  Created by Hunter Dobbelmann on 9/10/22.
+//
+
+import SwiftUI
+
+@main
+struct WaitTimesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
